ed8850d1c8d8631e6fe18b6e7066accc37897e06
iamfuntime <fun71m3@gmail.com>
2017-11-02 00:46:32 +0000

    enabled threading

--- src/enumerate_scan.py
+++ src/enumerate_scan.py
@@ -5,6 +5,8 @@ import socket
 import subprocess
 import multiprocessing
 from multiprocessing import Process,Queue
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
 
 from src.core import *
 
@@ -47,162 +49,173 @@ def id_services(scandir):
         print("{0}[+]{1} Running Detailed Nmap Scans on {2} Services".format(bcolors.GREEN, bcolors.ENDC, str(len(service_dict))))
         
         
-def enumerate_scan(ipaddr, scandir, quiet):
-    for service in service_dict:
-        ports = service_dict[service]
-        if ('http' == service) or ('ssl/http' == service) or ('http' in service) or ('https' in service):
-            if ('nikto' in installed_tools):
-                NIKTO_SCAN = 'nikto -h {0} -p {1} | tee {2}/nikto_{1}.txt'.format(ipaddr, port, scandir, port)
-                if quiet is not True:
-                    print("{0}[+]{1} Running Nikto Scan on {2}:{3}".format(bcolors.GREEN, bcolors.ENDC, ipaddr, port))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess.call(NIKTO_SCAN, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+def tool_scans(ipaddr, scandir, service, port, quiet):
+    if ('http' == service) or ('ssl/http' == service) or ('http' in service) or ('https' in service):
+        if ('nikto' in installed_tools):
+            NIKTO_SCAN = 'nikto -h {0} -p {1} | tee {2}/nikto_{1}.txt'.format(ipaddr, port, scandir, port)
+            if quiet is not True:
+                print("{0}[+]{1} Running Nikto Scan on {2}:{3}".format(bcolors.GREEN, bcolors.ENDC, ipaddr, port))
             else: pass
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess.call(NIKTO_SCAN, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
 
-            if ('dirb' in installed_tools):
-                DIRB_SCAN = 'dirb http://{0}:{1}/ -o {2}/dirb_{1}.txt'.format(ipaddr, port, scandir)
-                if quiet is not True:
-                    print("{0}[+]{1} Running Dirb Scan on {2}:{3}".format(bcolors.GREEN, bcolors.ENDC, ipaddr, port))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess.call(DIRB_SCAN, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        if ('dirb' in installed_tools):
+            DIRB_SCAN = 'dirb http://{0}:{1}/ -o {2}/dirb_{1}.txt'.format(ipaddr, port, scandir)
+            if quiet is not True:
+                print("{0}[+]{1} Running Dirb Scan on {2}:{3}".format(bcolors.GREEN, bcolors.ENDC, ipaddr, port))
             else: pass
-            
-            if ('curl' in installed_tools):
-                CURL_SCAN = 'curl -i {0}'.format(ipaddr)
-                if quiet is not True:
-                    print("{0}[+]{1} Grabbing Web Headers on {2}:{3}".format(bcolors.GREEN, bcolors.ENDC, ipaddr, port))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess.call(CURL_SCAN, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess.call(DIRB_SCAN, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
+        
+        if ('curl' in installed_tools):
+            CURL_SCAN = 'curl -i {0}'.format(ipaddr)
+            if quiet is not True:
+                print("{0}[+]{1} Grabbing Web Headers on {2}:{3}".format(bcolors.GREEN, bcolors.ENDC, ipaddr, port))
             else: pass
-            
-        elif ('microsoft-ds' == service) or ('microsoft-ds' in service):
-            if ('enum4linux' in installed_tools):
-                ENUM_SCAN = 'enum4linux {0} | tee {1}/enum4linux.txt'.format(ipaddr, scandir)
-                if quiet is not True:
-                    print("{0}[+]{1} Running Enum4Linux on {0}".format(bcolors.GREEN, bcolors.ENDC, ipaddr))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess,call(ENUM_SCAN, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess.call(CURL_SCAN, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
+        
+    elif ('microsoft-ds' == service) or ('microsoft-ds' in service):
+        if ('enum4linux' in installed_tools):
+            ENUM_SCAN = 'enum4linux {0} | tee {1}/enum4linux.txt'.format(ipaddr, scandir)
+            if quiet is not True:
+                print("{0}[+]{1} Running Enum4Linux on {0}".format(bcolors.GREEN, bcolors.ENDC, ipaddr))
             else: pass
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess,call(ENUM_SCAN, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
+                    
+    elif ('telnet' == service) or ('telnet' in service):
+        if ('nc' in installed_tools):
+            BANNER_GRAB = 'nc -nv {0} {1} | tee {2}/telnet_banner.txt'.format(ipaddr, port, scandir)
+            if quiet is not True:
+                print("{0}[+]{1} Grabbing Telnet Banner".format(bcolors.GREEN, bcolors.ENDC))
+            else: pass
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess.call(BANNER_GRAB, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
+                    
+    elif ('smtp' == service) or ('smtp' in service):
+        if ('smtp-user-enum' in installed_tools):
+            SMTP_SCAN = 'smtp-user-enum -M VRFY -U usernames/top_shortlist.txt -t {0} -p {1}'.format(ipaddr, port)
+            if quiet is not True:
+                print("{0}[+]{1} Enumerating SMTP Users".format(bcolors.GREEN, bcolors.ENDC))
+            else: pass
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess.call(SMTP_SCAN, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
                         
-        elif ('telnet' == service) or ('telnet' in service):
-            if ('nc' in installed_tools):
-                BANNER_GRAB = 'nc -nv {0} {1} | tee {2}/telnet_banner.txt'.format(ipaddr, port, scandir)
-                if quiet is not True:
-                    print("{0}[+]{1} Grabbing Telnet Banner".format(bcolors.GREEN, bcolors.ENDC))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess.call(BANNER_GRAB, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+    elif ('snmp' == service) or ('snmp' in service):
+        if ('onesixtyone' in installed_tools):
+            SNMP_SCAN = 'onesixtyone {0}'.format(ipaddr)
+            if quiet is not True:
+                print("{0}[+]{1} Enumerating SNMP with OneSixtyOne".format(bcolors.GREEN, bcolors.ENDC))
             else: pass
-                        
-        elif ('smtp' == service) or ('smtp' in service):
-            if ('smtp-user-enum' in installed_tools):
-                SMTP_SCAN = 'smtp-user-enum -M VRFY -U usernames/top_shortlist.txt -t {0} -p {1}'.format(ipaddr, port)
-                if quiet is not True:
-                    print("{0}[+]{1} Enumerating SMTP Users".format(bcolors.GREEN, bcolors.ENDC))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess.call(SMTP_SCAN, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess.call(SNMP_SCAN, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
+                    
+        if ('snmpwalk' in installed_tools):
+            SNMP_SCAN = 'snmpwalk -c public -v1 {0} | tee {1}/snmpwalk.txt'.format(ipaddr, scandir)
+            if quiet is not True:
+                print("{0}[+]{1} Enumerating SNMP with SNMPWalk".format(bcolors.GREEN, bcolors.ENDC))
             else: pass
-                            
-        elif ('snmp' == service) or ('snmp' in service):
-            if ('onesixtyone' in installed_tools):
-                SNMP_SCAN = 'onesixtyone {0}'.format(ipaddr)
-                if quiet is not True:
-                    print("{0}[+]{1} Enumerating SNMP with OneSixtyOne".format(bcolors.GREEN, bcolors.ENDC))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess.call(SNMP_SCAN, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess.call(SNMP_SCAN, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
+    elif ('ssh' == service) or ('ssh' in service):
+        if ('hydra' in installed_tools):
+            SSH_SCAN = 'hydra -f -V -t 1 -l root -P /usr/share/wordlists/rockyou.txt -s {0} {1} ssh'.format(
+                port, ipaddr)
+            if quiet is not True:
+                print("{0}[+]{1} Running Hydra Against SSH".format(bcolors.GREEN, bcolors.ENDC))
             else: pass
-                        
-            if ('snmpwalk' in installed_tools):
-                SNMP_SCAN = 'snmpwalk -c public -v1 {0} | tee {1}/snmpwalk.txt'.format(ipaddr, scandir)
-                if quiet is not True:
-                    print("{0}[+]{1} Enumerating SNMP with SNMPWalk".format(bcolors.GREEN, bcolors.ENDC))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess.call(SNMP_SCAN, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess.call(SSH_SCAN, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
+        
+        if ('medusa' in installed_tools):
+            SSH_SCAN = 'medus -u root -p /usr/share/wordlists/rockyou.txt -e ns -h {0} - {1} -M ssh'.format(
+                ipaddr, port)
+            if quiet is not True:
+                print("{0}[+]{1} Running Medusa Against SSH".format(bcolors.GREEN, bcolors.ENDC))
             else: pass
-        elif ('ssh' == service) or ('ssh' in service):
-            if ('hydra' in installed_tools):
-                SSH_SCAN = 'hydra -f -V -t 1 -l root -P /usr/share/wordlists/rockyou.txt -s {0} {1} ssh'.format(
-                    port, ipaddr)
-                if quiet is not True:
-                    print("{0}[+]{1} Running Hydra Against SSH".format(bcolors.GREEN, bcolors.ENDC))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess.call(SSH_SCAN, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
-            else: pass
+            with open(os.devnull, 'w') as FNULL:
+                try:
+                    subprocess.call(SSH_SCAN, stdout=FNULL, shell=True)
+                except subprocess.CalledProcessError as e:
+                    raise RuntimeError("command '{}' return with error (code {}): {}".format(
+                        e.cmd, e.returncode, e.output))
+                except KeyboardInterrupt:
+                    print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
+        else: pass
             
-            if ('medusa' in installed_tools):
-                SSH_SCAN = 'medus -u root -p /usr/share/wordlists/rockyou.txt -e ns -h {0} - {1} -M ssh'.format(
-                    ipaddr, port)
-                if quiet is not True:
-                    print("{0}[+]{1} Running Medusa Against SSH".format(bcolors.GREEN, bcolors.ENDC))
-                else: pass
-                with open(os.devnull, 'w') as FNULL:
-                    try:
-                        subprocess.call(SSH_SCAN, stdout=FNULL, shell=True)
-                    except subprocess.CalledProcessError as e:
-                        raise RuntimeError("command '{}' return with error (code {}): {}".format(
-                            e.cmd, e.returncode, e.output))
-                    except KeyboardInterrupt:
-                        print("{0}[!]{1} Scan Cancelled! Moving On!".format(bcolors.RED, bcolors.ENDC))
-            else: pass
+            
+def enumerate_scan(ipaddr, scandir, quiet):
+    id_services(scandir)
+    jobs = []
+    for service in service_dict:
+        for ports in service_dict[service]:
+            port = port.split('/')[0]
+            jobs.append((ipaddr, scandir, service, port, quiet))
+            
+    pool = ThreadPool(4)
+    pool.map(tool_scans, jobs)
+        
